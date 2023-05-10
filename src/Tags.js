e88777f29881afb43dffcf8e9cf3245f8d8ebc0a
Ozoani Chidubem <flyinglego153971@gmail.com>
2023-05-10 23:27:57 +0100

    Gave the grids some tweaking to improve aesthetics

--- src/Tags.js
+++ src/Tags.js
@@ -6,10 +6,10 @@ function Tags(){
     <div className="w-[90%] h-fit flex items-center">
 
 <div className="w-[40%] h-[100px] flex justify-center items-center">
-    <div className="font-custom1 text-xl font-semibold">Categories &gt;</div>
+    <div className="font-custom1 text-xl font-semibold">Categories</div>
 </div>
 
-<div className="w-[60%] h-fit flex flex-wrap flex-row content-center justify-center items-center text-white font-semibold text-sm">
+<div className="w-[60%] h-fit flex flex-wrap flex-row content-center justify-center items-center text-white font-semibold text-sm ">
     <div className="w-fit h-fit px-[5px] py-[3px] bg-stone-400 rounded-md m-[2px]">Kawaii</div>
     <div className="w-fit h-fit px-[5px] py-[3px] bg-stone-400 rounded-md m-[2px]">Bohemian</div>
     <div className="w-fit h-fit px-[5px] py-[3px] bg-stone-400 rounded-md m-[2px]">Modern</div>
