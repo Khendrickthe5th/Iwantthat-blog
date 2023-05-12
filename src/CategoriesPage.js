264d0de4f7903439c10412d780088324b57d02df
Ozoani Chidubem <flyinglego153971@gmail.com>
2023-05-12 18:05:36 +0100

    Styled the nav system for mobile devices with icons and tweaked the grids more

---
+++ src/CategoriesPage.js
@@ -0,0 +1,7 @@
+function CategoriesPage(){
+    <div className="w-[100%] h-fit">
+        <div className="w-[90%] h-fit bg-red-600"></div>
+    </div>
+}
+
+export default CategoriesPage;
--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,9 @@ import {useRef} from "react";
 // import ReactDOM from "react-dom/client";
 import './index.css';
 import Hamburger from './icons/Hamburger-Menu.svg';
+import Categories from './icons/Categories.svg';
+import About from './icons/About.svg';
+import FAQ from './icons/FAQ.svg';
 
 
 function Header() {
@@ -39,10 +42,23 @@ else{
         <div onClick={selected} className=" w-[45px] h-[45px] rounded-full flex"><img className="w-[20px] m-auto" src={Hamburger} alt="Hamburger Svg" /></div>
         </div>
 
-        <div ref={sideMenu} className="w-full h-[100%] bg-purple-400 absolute hidden top-[75px] m-0 p-0 z-[1200]">
-            <span className="w-fit mx-3">Categories</span>
-            <span className="w-fit mx-3">About</span>
-            <span className="w-fit mx-3">FAQ</span>  
+        <div ref={sideMenu} className="w-full h-[100%] bg-white absolute hidden top-[75px] m-0 p-0 z-[1200]">
+            <div className="w-[100%] h-[70px] flex justify-start items-center ">
+                <div className="m-[20px]"><img className="w-[30px] h-[30px]" width="100%" height="100%" color="red" src={Categories} alt="Categories icon"/> </div>
+                <a href="#"><span className="w-fit mx-3 text-xl font-semibold">Categories</span></a>
+            </div>
+
+            <div className="w-[100%] h-[70px] flex justify-start items-center ">
+                <div className="m-[20px]"><img className="w-[30px] h-[30px]" width="100%" height="100%" color="red" src={About} alt="Categories icon"/> </div>
+                <a href="#"><span className="w-fit mx-3 text-xl font-semibold">About</span></a>
+            </div>
+
+            <div className="w-[100%] h-[70px] flex justify-start items-center ">
+                <div className="m-[20px]"><img className="w-[30px] h-[30px]" width="100%" height="100%" color="red" src={FAQ} alt="Categories icon"/> </div>
+                <a href="#"><span className="w-fit mx-3 text-xl font-semibold">FAQs</span></a>
+            </div>
+
+            <div className="w-[100%] h-fit mt-[250px] text-xs"><p className="w-fit h-fit m-auto">I Want That 2023 C</p></div>  
         </div>
 
 
